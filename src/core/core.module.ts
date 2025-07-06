44143c693603f52e95997a1fdbe0e8dc3e330005
Nilton Dev <ngonzano.develop@gmail.com>
2025-07-06 15:46:41 -0500

    .

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,23 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppConfigService } from './config/app.config';
 import { CorsConfigService } from './config/cors.config';
 import { RoutesConfigService } from './config/routes.config';
 import { ProxyService } from './proxy/services/proxy.service';
 
 @Module({
   imports: [ConfigModule],
-  providers: [
-    ProxyService,
-    CorsConfigService,
-    AppConfigService,
-    RoutesConfigService,
-  ],
-  exports: [
-    ProxyService,
-    CorsConfigService,
-    AppConfigService,
-    RoutesConfigService,
-  ],
+  providers: [ProxyService, CorsConfigService, RoutesConfigService],
+  exports: [ProxyService, CorsConfigService, RoutesConfigService],
 })
 export class CoreModule {}
